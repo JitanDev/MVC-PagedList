b778f82564990990dba60d0fcc78afc90a766e20
JitanDev <37577349+JitanDev@users.noreply.github.com>
2019-06-07 15:01:30 +0530

    initial commit
    initial commit

---
+++ TestProj/TestProj/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestProj.Models;
+
+namespace TestProj.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 2;
+            int pageIndex = 1;
+            Employee employee = new Employee();
+            pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            IPagedList emps = null;
+            emps = employee.List().ToPagedList(pageIndex, pageSize);
+
+            return View(emps);
+        }
+    }
+}
---
+++ TestProj/TestProj/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace TestProj.Models
+{
+    public class Employee
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string Department { get; set; }
+        public DateTime JoiningDate { get; set; }
+
+        #endregion Properties
+
+        public IEnumerable<Employee> List()
+        {
+            IEnumerable<Employee> employees = new List<Employee>() {
+                new Employee(){ Id=1,Name="Tony Stark",Department="MCU", Designation="Iron Man", JoiningDate=DateTime.ParseExact("01/05/2008","dd/mm/yyyy",CultureInfo.InvariantCulture)},
+                new Employee(){ Id=2,Name="Steve Rozors",Department="MCU", Designation="Captain America", JoiningDate=DateTime.ParseExact("29/07/2011","dd/mm/yyyy",CultureInfo.InvariantCulture)},
+                new Employee(){ Id=3,Name="Thor",Department="MCU", Designation="Thor", JoiningDate=DateTime.ParseExact("29/04/2008","dd/mm/yyyy",CultureInfo.InvariantCulture)},
+                new Employee(){ Id=4,Name="Brue Banner",Department="MCU", Designation="Hulk", JoiningDate=DateTime.ParseExact("06/06/2008","dd/mm/yyyy",CultureInfo.InvariantCulture)}
+            };
+
+            return employees;
+        }
+    }
+}
